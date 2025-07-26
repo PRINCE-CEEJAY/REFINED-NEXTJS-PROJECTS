cff0b416cb5fa87eb5853262182860f4de5367b8
PRINCE-CEEJAY-NG <your_email@example.com>
2025-07-26 20:47:04 +0100

    Added firebase and building User authentication component

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-module.exports = {
-  swcMinify: true,
-  // disable react strict mode if not debugging
-  reactStrictMode: false,
-
-  // don't lint during build (use a separate script instead)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  typescript: {
-    ignoreBuildErrors: true, // temporarily to speed up
-  },
-};
